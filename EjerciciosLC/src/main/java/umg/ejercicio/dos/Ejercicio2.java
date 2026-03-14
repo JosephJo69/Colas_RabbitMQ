3e2e1d3758336c3647836e99a363b3a224c55876
Jose <jcruzf6@miumg.com.gt>
2026-03-14 15:02:38 -0600

    Tiempo

--- EjerciciosLC/src/main/java/umg/ejercicio/dos/Ejercicio2.java
+++ EjerciciosLC/src/main/java/umg/ejercicio/dos/Ejercicio2.java
@@ -1,5 +1,38 @@
 package umg.ejercicio.dos;
-
+import java.util.Arrays;
 public class Ejercicio2 {
+	public static void main(String[] args ) {
+		int[] numbers = {7,2,9,4,1,8};
+		int[] resultado = secondMinMax(numbers);
+		System.out.println("Arreglo original: " + Arrays.toString(numbers));
+		System.out.println("Resultado: " + Arrays.toString(resultado));
+	}
+	public static int[] secondMinMax(int[] numbers) {
+		
+		
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
 
+        for (int n : numbers) {
+           
+            if (n > max) {
+                secondMax = max; 
+                max = n;         
+            } else if (n > secondMax && n != max) {
+                secondMax = n;   
+            }
+
+           
+            if (n < min) {
+                secondMin = min; 
+                min = n;        
+            } else if (n < secondMin && n != min) {
+                secondMin = n;   //
+            }
+        }
+
+        return new int[]{secondMin, secondMax};
+    }
 }
