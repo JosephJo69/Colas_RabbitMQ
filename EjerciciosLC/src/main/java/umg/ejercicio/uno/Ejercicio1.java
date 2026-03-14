2a8405caab98690597aa66428ee9f6956308f08d
Jose <jcruzf6@miumg.com.gt>
2026-03-14 14:20:43 -0600

    Tiempo

--- EjerciciosLC/src/main/java/umg/ejercicio/uno/Ejercicio1.java
+++ EjerciciosLC/src/main/java/umg/ejercicio/uno/Ejercicio1.java
@@ -1,5 +1,32 @@
 package umg.ejercicio.uno;
 
 public class Ejercicio1 {
+	static int[] unocadapar = {1,2,3,4,5};
+	static int[] trescadaimparmenoscinco = {17,19,21,2,4,6};
+	static int[] cincocadacinco = {5,5,5,2,5,2};
+	
+	public static void main(String[] args) {
+		
+		System.out.println("La cantidad de pares en el primer arreglo es: ");
+        System.out.println(score(unocadapar, trescadaimparmenoscinco, cincocadacinco));
+        System.out.println("La cantidad de impares exepto 5 en el segundo arreglo es: ");}
+		
+	
+	  public static int score(int[] unocadapar, int[] trescadaimparmenoscinco, int[] cincocinco) {
+	        int count = 0;
+	        
+	        for (int i = 0; i < unocadapar.length; i++) {
+				if (unocadapar[i] % 2 == 0) {
+					count++;
+				}
+	        }	
+	        for (int i = 0; i < trescadaimparmenoscinco.length; i++) {
+	        				if (trescadaimparmenoscinco[i] % 2 != 0 && trescadaimparmenoscinco[i] - 5 < 0) {
+	        									
+	        				}
+	        }
+			      return count; 
+	    }
+	    
+	}
 
-}
