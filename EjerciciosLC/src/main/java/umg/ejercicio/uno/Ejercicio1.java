72441632b2c73f8e462ade0e8ea2750b48e0af18
Jose <jcruzf6@miumg.com.gt>
2026-03-14 14:41:34 -0600

    Tiempo

--- EjerciciosLC/src/main/java/umg/ejercicio/uno/Ejercicio1.java
+++ EjerciciosLC/src/main/java/umg/ejercicio/uno/Ejercicio1.java
@@ -1,32 +1,29 @@
 package umg.ejercicio.uno;
 
 public class Ejercicio1 {
-	static int[] unocadapar = {1,2,3,4,5};
-	static int[] trescadaimparmenoscinco = {17,19,21,2,4,6};
-	static int[] cincocadacinco = {5,5,5,2,5,2};
+	static int[] numbers = {0,1,2,3,4,5,6,7,8,9,10};
+	
 	
 	public static void main(String[] args) {
 		
-		System.out.println("La cantidad de pares en el primer arreglo es: ");
-        System.out.println(score(unocadapar, trescadaimparmenoscinco, cincocadacinco));
-        System.out.println("La cantidad de impares exepto 5 en el segundo arreglo es: ");}
+		System.out.println("El punteo es:  ");
+        System.out.println(score(numbers));}
+        
 		
 	
-	  public static int score(int[] unocadapar, int[] trescadaimparmenoscinco, int[] cincocinco) {
-	        int count = 0;
-	        
-	        for (int i = 0; i < unocadapar.length; i++) {
-				if (unocadapar[i] % 2 == 0) {
-					count++;
-				}
-	        }	
-	        for (int i = 0; i < trescadaimparmenoscinco.length; i++) {
-	        				if (trescadaimparmenoscinco[i] % 2 != 0 && trescadaimparmenoscinco[i] - 5 < 0) {
-	        									
-	        				}
-	        }
-			      return count; 
-	    }
-	    
-	}
+	public static int score(int[] numbers) {
+        int totalScore = 0;
 
+        for (int n : numbers) {
+            if (n == 5) {
+                totalScore += 5;
+            } else if (n % 2 == 0) {
+                totalScore += 1;
+            } else {
+                totalScore += 3; 
+            }
+        }
+
+        return totalScore;
+    }
+}
